6d5f77e6a138df4c0b703d5b3afb6edce9d138e6
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-09-27 21:27:57 +0300

    commit tree e2f52a0a1f221ab3c0282ad644e9c0ffc349e346

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+   
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
 end
